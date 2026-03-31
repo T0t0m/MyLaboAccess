12c0a082e26572a051ede06af39314d9d02d1544
Tom <tom.fabre31@gmail.com>
2026-03-31 09:29:00 +0200

    Update login.php

--- apps/backend/api/login.php
+++ apps/backend/api/login.php
@@ -7,9 +7,9 @@
 try {
     // Création de l'objet PDO pour établir la connexion
     $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName;charset=utf8mb4", $dbUser, $dbPass, [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-  ]);
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
     
 
     // Forcer l'encodage UTF-8 pour les échanges avec la base
